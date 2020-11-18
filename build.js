502f6da376338dd3004b7f298119459a8a2d26dc
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-11-18 15:06:46 +0530

    done or delete button added

--- build.js
+++ build.js
@@ -4,11 +4,11 @@ let Appurl = "https://ishjotsingh97.github.io/Vue-Todo-App/"
 
 var http = require('http'),fs = require('fs');
 
-
 fs.readFile('./index.html',async function (err, html) {
 	console.log(html)
 })
-    
-console.log("Script ended successfully")
 
 console.log(`App live at ${Appurl}`)
+
+console.log("Script ended successfully")
+
